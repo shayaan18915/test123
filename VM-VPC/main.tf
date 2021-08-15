0d76416df2413b478be8c89dcd601d6c0f80b5a2
shayaan18915 <shayaan18915@gmail.com>
2021-08-15 16:56:19 +0530

    first commit

---
+++ VM-VPC/main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = "maslab"
+  credentials = file("terraform.json")
+  region  = "europe-west2"
+  zone    = "europe-west2-a"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+
